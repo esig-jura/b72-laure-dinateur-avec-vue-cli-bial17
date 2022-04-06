af685c786cd7708b71f2022b94e4275d5e7c8479
alexandre.bilat <alexandre.bilat@divcom.ch>
2022-04-06 11:45:46 +0200

    mise en place composant Entete.vue et Pied.vue

---
+++ src/components/Entete.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <!-- Logo du site -->
+    <router-link class="logo" to="/">
+      <div class="nom">Laure Dinateur</div>
+      <div class="metier">Graphiste</div>
+    </router-link><nav><!-- Menu de navigation -->
+    <ul>
+      <li>
+        <!-- Lien actif : On ajoute la classe .actif au lien
+        représentant la page actuelle -->
+        <router-link to="/">Accueil</router-link>
+      </li>
+      <li>
+        <router-link to="/informations">Informations</router-link>
+      </li>
+      <li>
+        <router-link to="/contact">Me contacter</router-link>
+      </li>
+    </ul>
+  </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Entete',
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,19 +6,6 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue'),
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/About.vue'),
-  },
-  {
-    path: '/accueil',
     name: 'PageAccueil',
     component: () => import('../views/PageAccueil.vue'),
   },
